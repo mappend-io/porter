15a424744cce74fe7a11b227c9e98a69f55dd68e
Richard Moreland <richard@mappend.com>
2026-06-09 06:27:41 -0400

    Fix alias for old elevationPngContent layer definition entry

--- owt_streaming_service/src/layer_definition.rs
+++ owt_streaming_service/src/layer_definition.rs
@@ -39,7 +39,7 @@ pub struct LayerDefinition {
     pub content_transforms: BTreeSet<String>,
 
     // Like: `dtm/{FACE}/{LEVEL}/{COL}/{ROW}.tif`
-    #[serde(default, alias = "elevation_png_content")]
+    #[serde(default, alias = "elevationPngContent")]
     pub elevation_raster_content: Option<String>,
 }
 
